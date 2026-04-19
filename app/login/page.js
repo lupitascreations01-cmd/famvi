f90c74e1030d5a458f34c2ad4454dd110e904d00
lupitascreations01-cmd <lupitascreations.01@gmail.com>
2026-04-19 13:13:56 -0400

    google redirige a onboarding

--- app/login/page.js
+++ app/login/page.js
@@ -81,7 +81,7 @@ export default function Login() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/dashboard`
+          redirectTo: `${window.location.origin}/onboarding`
         }
       })
       console.log('data:', data)
