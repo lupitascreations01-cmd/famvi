9d6e49bea2a2b95c1ae9b10c6c6818d1a9888834
lupitascreations01-cmd <lupitascreations.01@gmail.com>
2026-04-18 23:15:38 -0400

    conexion supabase

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
 'use client'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 import { useState } from 'react'
 
